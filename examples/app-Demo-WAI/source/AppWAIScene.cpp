ecf46765b82e7cb915b243554b95aba55fe783c4
ghm1 <michael.goettlicher@bfh.ch>
2019-05-14 09:37:34 +0200

    Added shader to SLPoints material for correct visualization of points.

--- examples/app-Demo-WAI/source/AppWAIScene.cpp
+++ examples/app-Demo-WAI/source/AppWAIScene.cpp
@@ -1,22 +1,30 @@
 #include <AppWAIScene.h>
 
-AppWAIScene::AppWAIScene ()
+AppWAIScene::AppWAIScene()
 {
-    cameraNode = new SLCamera("Camera 1");
-    mapNode = new SLNode("map");
-    mapPC = new SLNode("MapPC");
-    mapMatchedPC = new SLNode("MapMatchedPC");
-    mapLocalPC = new SLNode("MapLocalPC");
-    keyFrameNode = new SLNode("KeyFrames");
+    cameraNode        = new SLCamera("Camera 1");
+    mapNode           = new SLNode("map");
+    mapPC             = new SLNode("MapPC");
+    mapMatchedPC      = new SLNode("MapMatchedPC");
+    mapLocalPC        = new SLNode("MapLocalPC");
+    keyFrameNode      = new SLNode("KeyFrames");
     covisibilityGraph = new SLNode("CovisibilityGraph");
-    spanningTree = new SLNode("SpanningTree");
-    loopEdges = new SLNode("LoopEdges");
+    spanningTree      = new SLNode("SpanningTree");
+    loopEdges         = new SLNode("LoopEdges");
+
     redMat = new SLMaterial(SLCol4f::RED, "Red");
+    redMat->program(new SLGLGenericProgram("ColorUniformPoint.vert", "Color.frag"));
+    redMat->program()->addUniform1f(new SLGLUniform1f(UT_const, "u_pointSize", 3.0f));
     greenMat = new SLMaterial(SLCol4f::GREEN, "Green");
+    greenMat->program(new SLGLGenericProgram("ColorUniformPoint.vert", "Color.frag"));
+    greenMat->program()->addUniform1f(new SLGLUniform1f(UT_const, "u_pointSize", 5.0f));
     blueMat = new SLMaterial(SLCol4f::BLUE, "Blue");
+    blueMat->program(new SLGLGenericProgram("ColorUniformPoint.vert", "Color.frag"));
+    blueMat->program()->addUniform1f(new SLGLUniform1f(UT_const, "u_pointSize", 4.0f));
+
     covisibilityGraphMat = new SLMaterial("YellowLines", SLCol4f::YELLOW);
-    spanningTreeMat = new SLMaterial("GreenLines", SLCol4f::GREEN);
-    loopEdgesMat = new SLMaterial("RedLines", SLCol4f::RED);
+    spanningTreeMat      = new SLMaterial("GreenLines", SLCol4f::GREEN);
+    loopEdgesMat         = new SLMaterial("RedLines", SLCol4f::RED);
 
     mapNode->addChild(mapPC);
     mapNode->addChild(mapMatchedPC);
